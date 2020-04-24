7f8123f26d8e8a27d139b08b73a297d8078ac23d
Noah Shuart <noahshuart@u.boisestate.edu>
2020-04-24 06:05:34 -0600

    Adjust XSS demo styling

--- XSS.php
+++ XSS.php
@@ -4,31 +4,44 @@
   header('X-XSS-Protection:0');
   $success = $username = $password = "";
   
-  if ($_GET){   
-    $username = $_GET["username"];
-    $password = $_GET["password"];   
+  if ($_POST){   
+    $username = $_POST["username"];
+    $password = $_POST["password"];   
     
-    if (!empty($_GET["username"])){
+    if (!empty($_POST["username"])){
       $success = "Good to see you again ".$username;
     }
   }
   
   //<?php $_SERVER['PHP_SELF']; ?>
 
+<?php if (!empty($success)): ?>
+  <div class="alert alert-success" role="alert">
+    <?= $success ?>
+  </div>
+<?php endif; ?>
 
-
-<h1 class="header">XSS Attack</h1>
-<form action="" method="get">
-  
-  <label for="username">Username </label>
-        <input type="text" id="username" name="username" value="<?= $username ?>">
-        
-  <label for="password">Password </label>
-        <input type="text" id="password" name="password" value="<?= $password ?>">
-
-  <button name="submit" type="submit" id="contactSubmit">Submit</button> 
-  
-  <div class="success"><?= $success ?></div>
+<h1 class="display-4">XSS Attack</h1>
+<form class="content-container mt-5 mx-auto" action="" method="post">
+  <div class="form-row">
+    <div class="form-group col-md-6">
+      <label for="username">Username</label>
+      <input
+          type="text"
+          id="username"
+          name="username"
+          class="form-control">
+    </div>
+    <div class="form-group col-md-6">
+      <label for="password">Password </label>
+      <input
+          type="text"
+          id="password"
+          name="password"
+          class="form-control">
+    </div>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
 </form>
 
 <?php require(__DIR__ . '/templates/foot.php');
